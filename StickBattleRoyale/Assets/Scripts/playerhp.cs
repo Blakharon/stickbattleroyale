34cb50b103f4aa66051762ad5cad23ed86dd1343
Seungdo-Justin Baek <sba111@sfu.ca>
2018-01-15 15:03:22 -0800

    healthbar added

---
+++ StickBattleRoyale/Assets/Scripts/playerhp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerhp : MonoBehaviour {
+
+	public const int max_health = 100;
+	public int current_health = max_health;
+	public RectTransform healthbar;
+
+	public void hp_damage (int amount){
+		current_health = current_health - amount;
+
+		if (current_health < 0) {
+			current_health = 0;
+		}
+
+		healthbar.sizeDelta = new Vector2 (current_health / 2, healthbar.sizeDelta.y);
+	}
+}
